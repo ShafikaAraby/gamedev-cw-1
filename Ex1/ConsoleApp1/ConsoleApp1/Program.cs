db7d4f0175f219033cd80a3dcd652c3a04c28581
ShafikaAraby <76395201+ShafikaAraby@users.noreply.github.com>
2020-12-21 13:16:34 +0300

    cw-1

---
+++ Ex1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String charaterName = "ahmed";
+            String superPower1 = "Steel shield";
+            String superPower2 = "Night vision";
+            int age = 15;
+            double speed = 1.65;
+            Console.WriteLine("My name is {0} my superpowers are {1} and {2} my age is {3} and speed {4}",
+                charaterName, superPower1, superPower2, age, speed);
+        }
+    }
+}
